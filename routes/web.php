0a6635a0f8096494976f9127061b6f3be3ce84cc
Nama Kamu <email@kamu.com>
2025-04-18 00:28:59 +0700

    sudah sudah sudah

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// untuk memanggil route PhotoController
+Route::resource('Photos', PhotoController::class)-> only
+(['index', 'show']);
+Route::resource('Photos', PhotoController::class)-> except
+(['create', 'store', 'update', 'delete']);
+
+//untuk memanggil file views/blog/hello.blade.php
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
+
+//untuk memanggil route controllers
 Route::get('/hello', [WelcomeController::class,'hello']);
 Route::get('/welcome', [PageController::class,'index']);
 Route::get('/about', [PageController::class,'about']);
 Route::get('/article/{id}', [PageController::class, 'article']);
-Route::resource('Photos', PhotoController::class);
+
 
 
 
